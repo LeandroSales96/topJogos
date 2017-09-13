8c2956fdb278718f469913cf9eecbfe5768551f5
Renato Ferreira <renatoferreira249@gmail.com>
2017-09-13 11:19:25 -0300

    começando a definir esqueleto

---
+++ jogo.py
@@ -0,0 +1,26 @@
+from graficos import Graficos
+
+def __init__(self):
+    self.graficos =
+    self.animais = criaAnimaisInicial()
+    self.matrizAnimais = self.criaMatrizAnimaisInicial(animais)
+    self.matrizRastros = [[0]*64]*64
+
+def criaAnimaisInicial():
+    return animais
+
+def criaMatrizAnimaisInicial(animais):
+    matrizAnimais = [[None]*64]*64
+
+    return matrizAnimais
+
+def verificaFimJogo():
+    fim = False 
+    return fim
+
+def executaJogo():
+    while(not verificaFimJogo()):
+        acoes = map(,animais)
+        atualizaAcoes()
+
+
