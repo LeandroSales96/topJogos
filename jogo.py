419d64e3b5bc0df5f444c0d3c8d8688458c5ccea
rena1234 <renatoferreira249@gmail.com>
2017-09-14 13:07:18 -0300

    começou aula do raul

--- jogo.py
+++ jogo.py
@@ -1,10 +1,13 @@
 from graficos import Graficos
 from copy import deepcopy
+from peixe import Peixe
+from tubarao import Tubarao
 
 def __init__(self):
     self.executaJogo()
 
 def criaAnimaisInicial():
+	#tuple (obj,id,linha,coluna)
     return animais
 
 def criaMatrizAnimaisInicial(animais):
@@ -16,10 +19,15 @@ def verificaFimJogo():
     fim = False 
     return fim
 
-def retornaRastrosAtualizados(matrizRastros):
-    def subtrai1Array(array):
-        arrayMenos1 = map(lambda x : x - 1, array)
-    return map(lambda x : subtrai1Array(x), matrizRastros)
+def atualizaAmbiente(ambiente,):
+	
+	def retornaRastrosAtualizados(matrizRastros):
+	    def subtrai1Array(array):
+	        arrayMenos1 = map(lambda x : x - 1, array)
+	    return map(lambda x : subtrai1Array(x), matrizRastros)
+	
+	def retornaMatrizAnimaisAtualizada(matrizAnimais):
+		return matrizAnimaisAtualizada
 """
 def retornaMatrizAnimaisAtualizada(acoes, matrizAnimais):
     return matrizAnimaisAtualizada
@@ -29,11 +37,19 @@ def executaJogo(self):
     animais = criaAnimaisInicial()
     matrizAnimais = criaMatrizAnimaisInicial(animais)
     matrizRastros = [[0]*64]*64
-    ambiente = (matrizAnimais,matrizRastros)
+    ambiente = (matrizAnimais, matrizRastros)
+    def retornaIdsMortos(acoes):
+    def retornaIdsFecundados(acoes):
+    def retornaOjtsNascidos(acoes):
+
     while(not self.verificaFimJogo(matrizAnimais)):
         animaisProxRodada = deepcopy(animais)
+        idsMortos = []
+        idsFecundados = []
+        objtsNascidos = []
         for animal in animais:
-            ambiente = animal.
+            acoes = animal.acoesIntraBloco(ambiente)
+
 
         """
         acoes = map(lambda x : x.acoesIntraBloco(ambiente) ,animais)
